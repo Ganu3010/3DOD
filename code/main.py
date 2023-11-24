1d6c08d65c4111f97f38f01adb39da338f18e215
ganu3010 <manas0sewatkar@gmail.com>
2023-11-25 00:56:58 +0530

    Dummy api added

--- code/main.py
+++ code/main.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h1>Hello Excelize</h1>"
+books = [
+    {
+        'name': 'Myth of Sisyphus',
+        'author':"Albert Camus"
+    },
+    {
+        'name': 'The Trouble with Being Born',
+        'author': 'E. M. Cioran'
+    }
+]
+
+# @app.route('/')
+# def home():
+#     return "<h1>Hello Excelize</h1>"
+
+@app.route('/', methods=['POST', 'GET'])
+def result():
+    if request.method == 'GET':
+        return jsonify(books)
+    elif request.method == 'POST':
+        name = request.form['name']
+        author = request.form['author']
+        books.append({'name': name, 'author': author})
+        return jsonify(books)
+    
+    else:
+        return 'Nothing', 404
+
 
 if __name__=="__main__":
     app.run(debug=True)
