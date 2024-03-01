1068fb9b5a44bfdec5355f851d735b0536779a26
ganu3010 <manas0sewatkar@gmail.com>
2024-03-01 16:52:14 +0530

    removing redundancies

--- code/input.py
+++
@@ -1,8 +0,0 @@
-import open3d as o3d
-import numpy as np
-import os
-from main import classify, to_pcd
-
-for file in os.listdir('static/input/'):
-    pcd = o3d.io.read_point_cloud('static/input/'+file)
-    o3d.visualization.draw_geometries([pcd], window_name = file)
--- test.py
+++
@@ -1,7 +0,0 @@
-import open3d as o3d
-
-pcd = o3d.io.read_point_cloud('website/static/input/Area_1_hallway_4.ply')
-
-# o3d.io.write_point_cloud('website/static/input/Area_1_hallway_4.ply', pcd)
-
-o3d.visualization.draw_geometries([pcd])
